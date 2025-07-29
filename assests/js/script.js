a27586a301af84b14a1ef989edca4a09701dadab
Akshay Labhade <akshay.labhade98@gmail.com>
2025-07-29 13:56:47 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Portfolio | Pallavi Pandey";
+        document.title = "Portfolio | Akshay Labhade";
         $("#favicon").attr("href","assests/images/favicon.ico");
     }
     else {
